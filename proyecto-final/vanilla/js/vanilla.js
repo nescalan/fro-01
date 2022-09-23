68bc3d210b233456a5d8714193dad6ceb57e80cb
Ing. Nelson Gonzalez Escalante <nescalan@gmail.com>
2022-09-23 16:27:19 -0600

    finished: navbar mobile

---
+++ proyecto-final/vanilla/js/vanilla.js
@@ -0,0 +1,7 @@
+const openMenu = () => {
+  document.querySelector(".navbar-mobile").style.right = "-20%";
+};
+
+const closeMenu = () => {
+  document.querySelector(".navbar-mobile").style.right = "-170%";
+};
